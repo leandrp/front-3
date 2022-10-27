e594069eb844a3f15c3942c9017f83d517dd2afc
leandrp <ferreira.leandro.p@gmail.com>
2022-10-26 22:31:33 -0300

    update-local-storage
    atualização da pasta local no git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { InstagramTimeLine } from './challenges/InstagramTimeLine'
+import { QuartaAula } from './lessons/QuartaAula'
+import { TerceiraAula } from './lessons/TerceiraAula'
+
+function App() {
+
+  return (
+    <QuartaAula />
+  )
+
+}
+
+export default App
